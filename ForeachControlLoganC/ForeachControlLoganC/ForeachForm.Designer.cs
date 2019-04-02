535a09793c9fa05dee1c3ddd5a7b696a25b5f9f4
loganschool <logan.cantin@ocsbstudent.ca>
2019-04-02 12:58:50 -0400

    Foreach

---
+++ ForeachControlLoganC/ForeachControlLoganC/ForeachForm.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace ForeachControlLoganC
+{
+    partial class frmForeach
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRed = new System.Windows.Forms.Button();
+            this.btnBlue = new System.Windows.Forms.Button();
+            this.btnGreen = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // btnRed
+            // 
+            this.btnRed.Location = new System.Drawing.Point(26, 12);
+            this.btnRed.Name = "btnRed";
+            this.btnRed.Size = new System.Drawing.Size(75, 23);
+            this.btnRed.TabIndex = 0;
+            this.btnRed.Text = "Red";
+            this.btnRed.UseVisualStyleBackColor = true;
+            this.btnRed.Click += new System.EventHandler(this.btnRed_Click);
+            // 
+            // btnBlue
+            // 
+            this.btnBlue.Location = new System.Drawing.Point(188, 12);
+            this.btnBlue.Name = "btnBlue";
+            this.btnBlue.Size = new System.Drawing.Size(75, 23);
+            this.btnBlue.TabIndex = 1;
+            this.btnBlue.Text = "Blue";
+            this.btnBlue.UseVisualStyleBackColor = true;
+            this.btnBlue.Click += new System.EventHandler(this.btnBlue_Click);
+            // 
+            // btnGreen
+            // 
+            this.btnGreen.Location = new System.Drawing.Point(107, 12);
+            this.btnGreen.Name = "btnGreen";
+            this.btnGreen.Size = new System.Drawing.Size(75, 23);
+            this.btnGreen.TabIndex = 2;
+            this.btnGreen.Text = "Green";
+            this.btnGreen.UseVisualStyleBackColor = true;
+            this.btnGreen.Click += new System.EventHandler(this.btnGreen_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(29, 103);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 4;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(123, 61);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(85, 17);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // frmForeach
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGreen);
+            this.Controls.Add(this.btnBlue);
+            this.Controls.Add(this.btnRed);
+            this.Name = "frmForeach";
+            this.Text = "Foreach with Control Object by  Logan C";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRed;
+        private System.Windows.Forms.Button btnBlue;
+        private System.Windows.Forms.Button btnGreen;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+    }
+}
+
---
+++ ForeachControlLoganC/ForeachControlLoganC/ForeachForm.cs
@@ -0,0 +1,62 @@
+﻿/*
+ * Created by: Logan Cantin 
+ * Created on: April 2, 2019
+ * Created for: ICS3U Programming
+ * Daily Assignment – Day #27 - Control Objects
+ * This program changes the background color of all non-button objects in the form
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ForeachControlLoganC
+{
+    public partial class frmForeach : Form
+    {
+        public frmForeach()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRed_Click(object sender, EventArgs e)
+        {
+            // Turning all objects except labels red
+            foreach (Control aControlObject in this.Controls)
+            {
+                //Checking if its a label
+                if (aControlObject.GetType() == typeof(Button))
+                    continue;
+                //not a label, so change color
+                aControlObject.BackColor = Color.Red;
+            }
+        }
+
+        // Same structure as above
+        private void btnGreen_Click(object sender, EventArgs e)
+        {
+            foreach (Control aControlObject in this.Controls)
+            {
+                if (aControlObject.GetType() == typeof(Button))
+                    continue;
+                aControlObject.BackColor = Color.Green;
+            }
+        }
+
+        private void btnBlue_Click(object sender, EventArgs e)
+        {
+            foreach (Control aControlObject in this.Controls)
+            {
+                if (aControlObject.GetType() == typeof(Button))
+                    continue;
+                aControlObject.BackColor = Color.Blue;
+            }
+        }
+    }
+}
