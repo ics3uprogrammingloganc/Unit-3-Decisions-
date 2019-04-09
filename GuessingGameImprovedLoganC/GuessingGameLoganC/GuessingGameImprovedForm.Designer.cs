1290abd6e1b505279acc336bb43dc63fb0718675
loganschool <logan.cantin@ocsbstudent.ca>
2019-04-09 13:01:36 -0400

    GuessingGameImproved

---
+++ GuessingGameImprovedLoganC/GuessingGameLoganC/GuessingGameImprovedForm.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace GuessingGameLoganC
+{
+    partial class frmGuessingGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPrompt = new System.Windows.Forms.Label();
+            this.tbxGuess = new System.Windows.Forms.TextBox();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblPrompt
+            // 
+            this.lblPrompt.AutoSize = true;
+            this.lblPrompt.Enabled = false;
+            this.lblPrompt.Location = new System.Drawing.Point(26, 22);
+            this.lblPrompt.Name = "lblPrompt";
+            this.lblPrompt.Size = new System.Drawing.Size(176, 13);
+            this.lblPrompt.TabIndex = 0;
+            this.lblPrompt.Text = "Guess a number between 1 and 10:";
+            // 
+            // tbxGuess
+            // 
+            this.tbxGuess.Enabled = false;
+            this.tbxGuess.Location = new System.Drawing.Point(208, 19);
+            this.tbxGuess.Name = "tbxGuess";
+            this.tbxGuess.Size = new System.Drawing.Size(100, 20);
+            this.tbxGuess.TabIndex = 1;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Enabled = false;
+            this.btnSubmit.Location = new System.Drawing.Point(29, 61);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 2;
+            this.btnSubmit.Text = "Guess";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Enabled = false;
+            this.lblResult.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.Location = new System.Drawing.Point(131, 61);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 25);
+            this.lblResult.TabIndex = 3;
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.Location = new System.Drawing.Point(53, 146);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(279, 99);
+            this.btnPlay.TabIndex = 4;
+            this.btnPlay.Text = "Play";
+            this.btnPlay.UseVisualStyleBackColor = true;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // frmGuessingGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(388, 257);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.tbxGuess);
+            this.Controls.Add(this.lblPrompt);
+            this.Name = "frmGuessingGame";
+            this.Text = "Guessing Game by Logan Cantin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPrompt;
+        private System.Windows.Forms.TextBox tbxGuess;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnPlay;
+    }
+}
+
---
+++ GuessingGameImprovedLoganC/GuessingGameLoganC/GuessingGameImprovedForm.cs
@@ -0,0 +1,83 @@
+﻿/*
+* Created by: Logan C  
+* Created on: April 9, 2019
+* Created for: ICS3U Programming
+* Daily Assignment – Day #17 - Number guessing game
+* This program lets people guess a random number. It lets you know if you got it correct or not.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace GuessingGameLoganC
+{
+    public partial class frmGuessingGame : Form
+    {
+        public int SECRET_NUM;
+        Random randomIntergerGenerator = new Random();
+
+
+        public frmGuessingGame()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            // Convert text in text box to interger
+            int userGuess = Convert.ToInt32(tbxGuess.Text);
+
+            // Is user's guess correct?
+            if (userGuess == SECRET_NUM)
+            {
+                // User wins
+                lblResult.Text = "You guessed correctly!";        
+                //System.Media.SoundPlayer player = new System.Media.SoundPlayer("correct.mp3");
+                //player.Play();
+                UserWin();
+            }
+            else
+            {
+                //user Loses
+                lblResult.Text = "You guessed incorrectly.";
+                //System.Media.SoundPlayer player = new System.Media.SoundPlayer("incorrect.mp3");
+                //player.Play();
+            }
+        }
+
+        //User wins. Disable all controls except the play button
+        private void UserWin()
+        {
+            foreach (Control control in this.Controls)
+            {
+                if (control.Name != "btnPlay")
+                {
+                    control.Enabled = false;
+                }
+
+            }
+            btnPlay.Enabled = true;
+        }
+
+        // Play button was clicked. enable all controls, disable play button, and generate a new secret number
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            SECRET_NUM = randomIntergerGenerator.Next(1, 11);
+            foreach (Control control in this.Controls)
+            {
+                if (control.Name != "btnPlay")
+                {
+                    control.Enabled = true;
+                }
+
+            }
+            btnPlay.Enabled = false;
+        }
+    }
+}
