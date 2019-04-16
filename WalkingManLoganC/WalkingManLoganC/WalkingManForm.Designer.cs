e56da5764278806905c2a23a94368c62edb4dd95
loganschool <logan.cantin@ocsbstudent.ca>
2019-04-16 13:11:19 -0400

    WalkingMan

---
+++ WalkingManLoganC/WalkingManLoganC/WalkingManForm.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace WalkingManLoganC
+{
+    partial class frmWalkingMan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbxWalkingMan = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxWalkingMan)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbxWalkingMan
+            // 
+            this.pbxWalkingMan.Image = global::WalkingManLoganC.Properties.Resources.walk1;
+            this.pbxWalkingMan.InitialImage = global::WalkingManLoganC.Properties.Resources.walk1;
+            this.pbxWalkingMan.Location = new System.Drawing.Point(117, 89);
+            this.pbxWalkingMan.Name = "pbxWalkingMan";
+            this.pbxWalkingMan.Size = new System.Drawing.Size(182, 194);
+            this.pbxWalkingMan.TabIndex = 0;
+            this.pbxWalkingMan.TabStop = false;
+            this.pbxWalkingMan.Click += new System.EventHandler(this.pbxWalkingMan_Click);
+            // 
+            // frmWalkingMan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(410, 316);
+            this.Controls.Add(this.pbxWalkingMan);
+            this.Name = "frmWalkingMan";
+            this.Text = "Walking Man by Logan C";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxWalkingMan)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbxWalkingMan;
+    }
+}
+
---
+++ WalkingManLoganC/WalkingManLoganC/WalkingManForm.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WalkingManLoganC
+{
+    public partial class frmWalkingMan : Form
+    {
+        public frmWalkingMan()
+        {
+            InitializeComponent();
+        }
+
+        private void pbxWalkingMan_Click(object sender, EventArgs e)
+        {
+            Image walkingman = Properties.Resources.walk1;
+
+            for (int i = 0; i < 10; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        walkingman = Properties.Resources.walk1;
+                        break;
+                    case 1:
+                        walkingman = Properties.Resources.walk2;
+                        break;
+                    case 2:
+                        walkingman = Properties.Resources.walk3;
+                        break;
+                    case 3:
+                        walkingman = Properties.Resources.walk4;
+                        break;
+                    case 4:
+                        walkingman = Properties.Resources.walk5;
+                        break;
+                    case 5:
+                        walkingman = Properties.Resources.walk6;
+                        break;
+                    case 6:
+                        walkingman = Properties.Resources.walk7;
+                        break;
+                    case 7:
+                        walkingman = Properties.Resources.walk8;
+                        break;
+                    case 8:
+                        walkingman = Properties.Resources.walk9;
+                        break;
+                    case 9:
+                        walkingman = Properties.Resources.walk10;
+                        break;
+                }
+                pbxWalkingMan.Image = walkingman;
+
+                this.Refresh();
+
+                Thread.Sleep(50);
+            }
+
+            
+        }
+    }
+}
